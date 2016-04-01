f868a10325815f46611753cbc748e8a4d822ca73
DesmondMM <mutsakaradesmond@gmail.com>
2016-04-01 10:29:45 +0200

    Rewrote index.html code and app.ts imports

---
+++ app/ts/app.ts
@@ -0,0 +1,7 @@
+import {provide, Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser'
+import {ROUTER_DIRECTIVES,
+        ROUTER_PROVIDERS,
+        HashLocationStrategy,
+        RouteConfig,
+        } from 'angular2/router';
