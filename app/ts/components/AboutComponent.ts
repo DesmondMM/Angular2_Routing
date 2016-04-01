314ba00a847e5877174b66bdb7460517d6151eab
DesmondMM <mutsakaradesmond@gmail.com>
2016-04-01 10:44:06 +0200

    Wrote templates and controllers for home, about and contact

---
+++ app/ts/components/AboutComponent.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'about',
+    template:`
+    <div>
+        <h1>Welcome to the About page</h1>
+    </div>
+    `
+})
+export class AboutComponent {
+
+}
---
+++ app/ts/components/HomeComponent.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'home',
+    template:`
+    <div>
+        <h1>Welcome to the Homepage</h1>
+    </div>
+    `
+})
+export class HomeComponent {
+
+}
