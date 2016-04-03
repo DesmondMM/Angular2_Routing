3316f093396ad162baf24351b13be2aff25d9d74
DesmondMM <mutsakaradesmond@gmail.com>
2016-04-03 21:13:06 +0100

    Added a authentication service

---
+++ app/ts/services/AuthService.ts
@@ -0,0 +1,14 @@
+import {Injectable, provide} from 'angular2/core';
+
+@Injectable()
+export class AuthService {
+    login (user: string, string, password: string): boolean {
+        if (user === 'user' && password === 'password') {
+            localStorage.setItem('username', user);
+            return true;
+        }
+
+        return false;
+    }
+
+}
